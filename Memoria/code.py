0ad6681f53a607d965e076bcfd437903d201068f
GermangUGR <germangalmagro@correo.ugr.es>
2018-05-17 17:59:20 +0200

    Añadido capitulo 5

---
+++ Memoria/code.py
@@ -0,0 +1,14 @@
+def add_constraints(X, y, mat_const, nb_const, noise = 0, 
+	prop = False):
+def LCVQE(X, k, constraints, centroids, max_iter = 100)
+
+def TVClust(X, k, constraints, checked, max_iter = 100, 
+	isKeepL = 1, alpha0 = 1.2, stopThreshold=0.0005 )
+
+def CECM(X, k, constraints, alpha=1, rho2=100, bal=0.5, 
+	init=0)
+
+def COPKM(X, k, ml=[], cl=[], max_iter=300,  tol=1e-4,
+	initialization='rand')
+
+def RDPM(X, lamb, constraints, rate, xi0, max_iter)
