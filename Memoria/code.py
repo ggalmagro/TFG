bd4bae12a5be4950f120b3e5664286088e622a31
GermangUGR <germangalmagro@correo.ugr.es>
2018-06-06 12:41:48 +0200

    Correcciones en memoria

--- Memoria/code.py
+++ Memoria/code.py
@@ -6,7 +6,7 @@ def TVClust(X, K, constraints, max_iter = 300,
 	is_keep_l = 1, alpha0 = 1.2, stop_thr = 0.0005)
 
 def CEKM(X, K, constraints, max_iter = 300, alpha = 1, 
-	rho = 100, bal = 0.5, stop_thr = 1e-3, init = 'rand')
+	rho = 100, xi = 0.5, stop_thr = 1e-3, init = 'rand')
 
 def COPKM(X, K, constraints, max_iter = 300,  tol = 1e-4,
 	init = 'rand')
